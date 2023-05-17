1abf11348cd12a71ddf89f483fa91a2beee81700
TheHeiseDev <meizuset@gmail.com>
2023-05-17 19:33:11 +0300

    Добавил компонент пагинации и стилизовал

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ function App() {
       <Header />
       <Intro />
       <PostList />
-  
       <Footer />
     </>
   );
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./PostList.module.scss";
 import { Post } from "components/Post/Post";
+import { Pagination } from "@mui/material";
 
 export const PostList = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const handleSetPage = (number) => {
+    // smoothScroll(100);
+    // dispatch(setCurrentPage(page));
+    console.log(number);
+    setCurrentPage((prev) => (prev = currentPage));
+  };
+
   return (
     <div className={styles.posts}>
       <div className={styles.containerMini}>
@@ -10,6 +19,14 @@ export const PostList = () => {
           <Post />
           <Post />
           <Post />
+          <Pagination
+            color="secondary"
+            variant="outlined"
+            page={currentPage}
+            count={5}
+            className={styles.catalogPagination}
+            onChange={(_, num) => handleSetPage(num)}
+          />
         </div>
       </div>
     </div>
