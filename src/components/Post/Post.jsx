43761eea31a50adcf3c80bacc1aa4e9a73eca86c
TheHeiseDev <meizuset@gmail.com>
2023-05-18 11:45:56 +0300

    Исправил баги со стилями в компоненте PostList

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,22 +1,10 @@
 import React from "react";
 import styles from "./Post.module.scss";
-import image from "../../assets/bg-images/19.jpeg";
+
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 
-export const Post = () => {
-  const post = {
-    id: 1,
-    title: "Emil Bjarni - The Aesi",
-    description:
-      "Создайте подзаголовок поста: одно-два предложения, которые кратко передают содержание поста и побуждают продолжить чтение. Это текст... Создайте подзаголовок поста: одно-два предложения, которые кратко передают содержание поста и побуждают",
-    imageUrl: image,
-    views: 12,
-    comments: [],
-    date: "17.05.2023",
-    category: "ai",
-  };
-
+export const Post = ({ post }) => {
   return (
     <div className={styles.post}>
       <div className={styles.postWrapper}>
@@ -29,7 +17,7 @@ export const Post = () => {
             <span>1 мин.</span>
           </div>
           <div className={styles.postBody}>
-            <h2 className={styles.postTitle}>{post.title}</h2>
+            <h3 className={styles.postTitle}>{post.title}</h3>
             <div className={styles.descroptionContainer}>
               <p className={styles.postDescription}>{post.description}</p>
             </div>
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -5,24 +5,52 @@ import { Pagination } from "@mui/material";
 
 export const PostList = () => {
   const [currentPage, setCurrentPage] = useState(1);
+  const [posts, setPosts] = useState([]);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    const url = new URL(
+      `https://6440faa3792fe886a89abbd7.mockapi.io/posts?page=${page}&limit=5`
+    );
+
+    fetch(url, {
+      method: "GET",
+      headers: { "content-type": "application/json" },
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        // handle error
+      })
+      .then((posts) => {
+        setPosts(posts);
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  }, [page]);
+
   const handleSetPage = (number) => {
     // smoothScroll(100);
     // dispatch(setCurrentPage(page));
-    console.log(number);
-    setCurrentPage((prev) => (prev = currentPage));
+    window.scrollTo(0, 700);
+    setPage(number);
+    setCurrentPage(number);
   };
 
   return (
     <div className={styles.posts}>
       <div className={styles.containerMini}>
+        <h1>Лента новостей</h1>
         <div className={styles.postsWrapper}>
-          <Post />
-          <Post />
-          <Post />
+          {posts.map((post) => (
+            <Post key={post.id} post={post} />
+          ))}
           <Pagination
             color="secondary"
             variant="outlined"
-            page={currentPage}
+            page={page}
             count={5}
             className={styles.catalogPagination}
             onChange={(_, num) => handleSetPage(num)}
