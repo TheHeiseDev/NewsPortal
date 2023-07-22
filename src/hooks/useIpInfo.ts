59963931846c0b98325c53d169ace4f0aa6f81cd
TheHeiseDev <meizuset@gmail.com>
2023-07-22 13:31:34 +0300

    Переписал хук useIPInfo с использованием конструкции async/await

--- src/hooks/useIpInfo.ts
+++ src/hooks/useIpInfo.ts
@@ -1,23 +1,40 @@
 import { useState, useEffect } from "react";
 
 export function useIPInfo() {
-  const [ipAddress, setIPAddress] = useState<string>("");
-  const [country, setCountry] = useState<string>("");
+  const [ipAddress, setIPAddress] = useState<string>("0.0.0.0");
+  const [country, setCountry] = useState<string>("Неизвестно");
+
+  async function fetchData() {
+    try {
+      const ipResponse = await fetch("https://api.ipify.org?format=json");
+      if (!ipResponse.ok) throw new Error(`Ошибка HTTP: ${ipResponse.status}`);
+
+      const ipData = await ipResponse.json();
+      if (!ipData.ip) throw new Error(`Ошибка HTTP`);
+
+      setIPAddress(ipData.ip);
+
+      const countryResponse = await fetch(`https://ipapi.co/${ipData.ip}/country_name/`);
+      if (!countryResponse.ok) throw new Error(`Ошибка HTTP: ${countryResponse.status}`);
+
+      const country = await countryResponse.text();
+
+      if (!country || country === "Undefined")
+        throw new Error(`Что-то не так с ip адресом!`);
+
+      if (country) {
+        setCountry(country);
+      } else {
+        console.warn("Ошибка при получении страны");
+        throw new Error(`Ошибка запроса на получение данных о стране пользователя`);
+      }
+    } catch (error: any) {
+      console.error(`Произошла ошибка: ${error.message}`);
+    }
+  }
 
   useEffect(() => {
-    fetch("https://api.ipify.org?format=json")
-      .then((response) => response.json())
-      .then((data) => {
-        setIPAddress(data.ip);
-        return fetch(`https://ipapi.co/${data.ip}/country_name/`);
-      })
-      .then((response) => response.text())
-      .then((data) => {
-        setCountry(data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    fetchData();
   }, []);
 
   return { ipAddress, country };
